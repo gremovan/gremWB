0bd0ce982d92542b4387b3430c2058bfcacd65b3
gremovan <gremovan@gmail.com>
2017-01-25 21:23:25 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+$theme_name = "default_theme"; /* KITÖLTENDŐ! A THEME nevét ide kell írni */
+$theme_url= "themes/$theme_name";
+$theme_images_url ="$theme_url/images";
+$theme_modules_url = "$theme_url/modules";
+$modules_url = "modules";
+
+/* modulok elérési utak rövidítése */
+$navigation_header_modul = "$modules_url/navigation/navigation-header/tpl/nav_header.tpl";
+$navigation_sub_modul = "$modules_url/navigation/navigation-sub/tpl/nav_sub.tpl";
+$navigation_breadcrumb_modul = "$modules_url/navigation/breadcrumb/tpl/nav_bread.tpl";
+$article_head_modul = "$modules_url/article-head/tpl/art_head.tpl";
+$article_text_modul = "$modules_url/article-text/tpl/art_text.tpl";
+$article_image_modul = "$modules_url/article-image/tpl/art_image.tpl";
+$article_contentlist_modul = "$modules_url/article-contentlist/tpl/art_contlist.tpl";
+$article_tab_modul = "$modules_url/article-tabelement/tpl/art_tab.tpl";
+$social_pages_list = "$modules_url/socialnetwork/social-page-list/tpl/social_pages.tpl";
+$basic_slider = "$modules_url/basic-slider/tpl/slider_basic.tpl";
+$article_accordion_modul = "$modules_url/article-accordion/tpl/art_accordion.tpl";
+?>
